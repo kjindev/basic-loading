9167edabfbe240cced1e865f3b503150dfe9c109
kjin <pkjin.eng@gmail.com>
2023-11-25 18:58:56 +0900

    feat: plane component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import Canvas from "./lib/Canvas";
+import Plane from "./lib/Plane";
+
 function App() {
-  return <div>hello world</div>;
+  const option = {
+    width: "100px",
+    height: "100px",
+    borderRadius: "20px",
+    backgroundColor: "#000000",
+  };
+  return (
+    <Canvas>
+      <Plane option={option} />
+    </Canvas>
+  );
 }
 
 export default App;
--- src/lib/Plane.tsx
+++ src/lib/Plane.tsx
@@ -1,3 +1,31 @@
-export default function Plane() {
-  return <div>Plane</div>;
+import { ReactNode } from "react";
+
+interface PropsType {
+  option: {
+    width: string;
+    height: string;
+    borderRadius: string;
+    backgroundColor?: string;
+  };
+  className?: string;
+  children?: ReactNode;
+}
+
+export default function Plane({ option, className, children }: PropsType) {
+  const { width, height, borderRadius } = option;
+
+  const style = {
+    backgroundColor: "#2b3131",
+    width: width,
+    height: height,
+    borderRadius: borderRadius,
+    boxShadow: `inset 2px 2px 8px rgba(255, 255, 255, 0.3),
+        2px 2px 8px rgba(0, 0, 0, 0.2)`,
+  };
+
+  return (
+    <div id="plane" className={className} style={style}>
+      {children}
+    </div>
+  );
 }
