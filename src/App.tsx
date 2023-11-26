5a479d09bb69223deb66b27e7f4a4c5e3488efb7
kjin <pkjin.eng@gmail.com>
2023-11-26 16:06:05 +0900

    feat: Plane component style object

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import Canvas from "./lib/Canvas";
-import Plane from "./lib/Plane";
+import TestPage from "./TestPage";
 
 function App() {
-  const option = {
-    width: "100px",
-    height: "100px",
-    borderRadius: "20px",
-    backgroundColor: "#000000",
-  };
-  return (
-    <Canvas>
-      <Plane option={option} />
-    </Canvas>
-  );
+  return <TestPage />;
 }
 
 export default App;
--- src/lib/Plane.tsx
+++ src/lib/Plane.tsx
@@ -1,30 +1,46 @@
 import { ReactNode } from "react";
+import { lightenColor } from "./util/functions";
+
+type StringObject = {
+  [key: string]: string;
+};
 
 interface PropsType {
   option: {
     width: string;
     height: string;
-    borderRadius: string;
-    backgroundColor?: string;
+    borderRadius?: string;
+    color?: string;
   };
   className?: string;
+  style?: StringObject;
   children?: ReactNode;
 }
 
-export default function Plane({ option, className, children }: PropsType) {
-  const { width, height, borderRadius } = option;
+export default function Plane({
+  option,
+  className,
+  style,
+  children,
+}: PropsType) {
+  const { width, height, borderRadius, color } = option;
 
-  const style = {
+  const planeStyle = {
+    ...style,
     backgroundColor: "#2b3131",
+    background: `linear-gradient(to bottom right, ${lightenColor(
+      color ?? "#232323",
+      15
+    )}, ${color ?? "#232323"})`,
     width: width,
     height: height,
-    borderRadius: borderRadius,
+    borderRadius: borderRadius ?? "10px",
     boxShadow: `inset 2px 2px 8px rgba(255, 255, 255, 0.3),
         2px 2px 8px rgba(0, 0, 0, 0.2)`,
   };
 
   return (
-    <div id="plane" className={className} style={style}>
+    <div className={className} style={planeStyle}>
       {children}
     </div>
   );
