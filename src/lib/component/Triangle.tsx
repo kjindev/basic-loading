087e265f163e4c60487b989d40fb0964b81d5f48
kjin <pkjin.eng@gmail.com>
2023-11-29 15:02:03 +0900

    feat: Triangle component gradient and borderRadius

--- src/lib/component/Triangle.tsx
+++ src/lib/component/Triangle.tsx
@@ -1,11 +1,20 @@
-import "../../test.css";
+import "../../example/test.css";
+import { lightenColor } from "../util/functions";
 
 export default function Triangle() {
+  console.log(lightenColor("#818cf8", 6));
   return (
-    <div className="triangle">
-      <div className="tri-1"></div>
-      <div className="tri-2"></div>
-      <div className="tri-3"></div>
-    </div>
+    <>
+      <div className="triangle">
+        <div className="tri-1"></div>
+        <div className="tri-3"></div>
+        <div className="tri-2"></div>
+        {/* <div className="tri-center"></div> */}
+      </div>
+      {/* <div id="pool-container">
+        <div id="side-step"></div>
+        <div id="main-pool"></div>
+      </div> */}
+    </>
   );
 }
