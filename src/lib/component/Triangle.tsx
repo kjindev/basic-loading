d43509f07bca5e12196a560771bccc9a78bf934e
kjin <pkjin.eng@gmail.com>
2023-11-29 16:17:14 +0900

    feat: Triangle component gradient without inset shadow

--- src/lib/component/Triangle.tsx
+++ src/lib/component/Triangle.tsx
@@ -1,15 +1,12 @@
 import "../../example/test.css";
-import { lightenColor } from "../util/functions";
 
 export default function Triangle() {
-  console.log(lightenColor("#818cf8", 6));
   return (
     <>
       <div className="triangle">
         <div className="tri-1"></div>
-        <div className="tri-3"></div>
         <div className="tri-2"></div>
-        {/* <div className="tri-center"></div> */}
+        <div className="tri-3"></div>
       </div>
       {/* <div id="pool-container">
         <div id="side-step"></div>
