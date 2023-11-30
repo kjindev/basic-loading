7f3812d842b0df84be83cc351c1b88d267b8db2c
kjin <pkjin.eng@gmail.com>
2023-11-30 15:20:21 +0900

    feat: Triangle component inset css

--- src/lib/component/Triangle.tsx
+++ src/lib/component/Triangle.tsx
@@ -4,9 +4,12 @@ export default function Triangle() {
   return (
     <>
       <div className="triangle">
-        <div className="tri-1"></div>
         <div className="tri-2"></div>
+        <div className="tri-1"></div>
         <div className="tri-3"></div>
+        <div className="sample"></div>
+        <div className="sample2"></div>
+        <div className="sample3"></div>
       </div>
       {/* <div id="pool-container">
         <div id="side-step"></div>
