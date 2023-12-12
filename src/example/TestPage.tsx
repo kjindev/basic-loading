3895bfa7a1070d40eddeaf56a0309499d628d247
kjin <pkjin.eng@gmail.com>
2023-12-12 18:12:35 +0900

    feat: publish setting

--- src/example/TestPage.tsx
+++ src/example/TestPage.tsx
@@ -1,18 +1,19 @@
-import DotBasic from "../lib/component/DotBasic";
-import DotBounce from "../lib/component/DotBounce";
-import Fade from "../lib/component/Fade";
+// import DotBasic from "../lib/component/DotBasic";
+// import DotBounce from "../lib/component/DotBounce";
+// import Fade from "../lib/component/Fade";
+import React from "react";
 
 export default function TestPage() {
-  const option = {
-    size: 15,
-    color: "#232323",
-  };
+  // const option = {
+  //   size: 15,
+  //   color: "#232323",
+  // };
 
   return (
     <div>
       {/* <DotBasic option={option} /> */}
       {/* <DotBounce option={option} /> */}
-      <Fade>hello</Fade>
+      {/* <Fade>hello</Fade> */}
     </div>
   );
 }
