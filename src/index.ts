b19675e7c86be8fae4fbc20765439338b364997b
kjin <pkjin.eng@gmail.com>
2023-12-17 21:14:45 +0900

    feat: add lib folder and change directory

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DotBasic } from "./component/DotBasic";
-export { default as DotBounce } from "./component/DotBounce";
-export { default as Fade } from "./component/Fade";
---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+export { default as BasicDot } from "./component/BasicDot";
+export { default as BounceDot } from "./component/BounceDot";
+export { default as Fade } from "./component/Fade";
+export { default as ProgressBar } from "./component/ProgressBar";
