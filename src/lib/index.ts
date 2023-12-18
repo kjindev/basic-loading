480791c9d835d67bb2d8522220e08b6cfe48c445
kjin <pkjin.eng@gmail.com>
2023-12-18 13:47:12 +0900

    feat: update component index and tsconfig include

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,7 @@ export { default as BasicDot } from "./component/BasicDot";
 export { default as BounceDot } from "./component/BounceDot";
 export { default as Fade } from "./component/Fade";
 export { default as ProgressBar } from "./component/ProgressBar";
+export { default as Pulse } from "./component/Pulse";
+export { default as Skeleton } from "./component/Skeleton";
+export { default as Spinner } from "./component/Spinner";
+export { default as Wave } from "./component/Wave";
